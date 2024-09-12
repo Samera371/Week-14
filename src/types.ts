1ed23cd1ba79a6fdb6a7d444d03983922d983b37
Samera371 <bills.samera@gmail.com>
2024-09-11 23:39:40 -0400

    UPDATE FOR WEEK 15

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,13 @@
 export type Movie = {
-    id: number;
-    type: string;
-    title: string;
-    year: string;
-    genre: string[];
-    description: string;
-    imgUrl: string;
-    trailerUrl: string;
-    ratings: number[];
-  };
-  
+  id: number;
+  type: string;
+  title: string;
+  year: string;
+  genre: string[];
+  description: string;
+  imgUrl: string;
+  trailerUrl: string;
+  ratings: number[];
+};
+
+export type NewMovie = Omit<Movie, "id">;
